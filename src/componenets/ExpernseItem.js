80d232ac8efc4442da71b8e78b86f8f0cbbf8436
tara97 <taraprasad9090@gmail.com>
2022-12-24 15:46:07 +0530

    With dynamic Data

--- src/componenets/ExpernseItem.js
+++ src/componenets/ExpernseItem.js
@@ -1,12 +1,15 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+    const expenseDate = new Date(2022, 11, 25)
+    const expenseTitle = 'Entertainment Expense'
+    const expensePrice = 220
     return (
         <div className="expense-item">
-            <div>Date</div>
+            <div>{expenseDate.toDateString()}</div>
             <div className='expense-item__description'>
-                <h2>Hotel</h2>
-                <h2 className='expense-item__price'>33.56$</h2>
+                <h2>{expenseTitle}</h2>
+                <h2 className='expense-item__price'>${expensePrice}</h2>
             </div>
         </div>
     )
