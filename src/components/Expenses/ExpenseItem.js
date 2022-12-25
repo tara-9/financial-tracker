fc10d40ec25a7c738120859c6fa58032b2e04f3f
tara97 <taraprasad9090@gmail.com>
2022-12-25 23:59:01 +0530

    Adding State Object and User form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,23 @@
+import { useState } from 'react'
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 
+
 const ExpenseItem = props => {
+    
+    const [title, setTitle] = useState(props.title)
+
+    const onClickHandler = () => {
+        setTitle('Updated')
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <h2 className='expense-item__price'>${props.amount}</h2>
+                <button onClick={onClickHandler}>Update Title</button>
             </div>
         </Card>
     )
