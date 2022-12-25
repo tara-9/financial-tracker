159477509e3d412baddd6b6a887a72b966cfedfa
tara97 <taraprasad9090@gmail.com>
2022-12-25 18:10:20 +0530

    Using composition to avoid duplicate code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
+import Card from './Card'
 
 function ExpenseItem(props) {
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <h2 className='expense-item__price'>${props.amount}</h2>
             </div>
-        </div>
+        </Card>
     )
 }
 
