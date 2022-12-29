2a6d353c1d24dd5bfd46d98b0cad1a6bc5c5a4b1
tara97 <taraprasad9090@gmail.com>
2022-12-29 12:45:33 +0530

    Adding cancelqtion feature

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,16 +1,24 @@
+import { useState } from 'react'
 import './NewExpense.css'
 import ExpenseForm from './ExpenseForm'
-const NewExpense = (props) => {
 
+const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false)
     const saveExpenseHandler= savedExpense => {
         const expenseData = {
             ...savedExpense,
             id: Math.random().toString()
         }
         props.onAddExpenseHandler(expenseData)
+        finishEditingHandler()
+      
     }
+    const startEditingHandler = () => setIsEditing(true)
+    const finishEditingHandler = () => setIsEditing(false)
+
     return <div className='new-expense'>
-        <ExpenseForm onSaveExpenseHandler={saveExpenseHandler}/>
+        {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+        {isEditing && <ExpenseForm onSaveExpenseHandler={saveExpenseHandler} onCancel={finishEditingHandler} />}
     </div>
 }
 
